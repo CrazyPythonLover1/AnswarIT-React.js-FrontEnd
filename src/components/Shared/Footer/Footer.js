5c88867055a25475acb3081db11359f684cb7db4
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-12-11 20:12:40 +0600

    Footer complete

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -9,21 +9,18 @@ const Footer = () => {
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse " style={{maxWidth:"1200px",margin: "0 auto", padding: " 0 15px", }} id="navbarNav">
-  <a class="navbar-brand font-italic" href="#" style={{color: "white"}} >Networking People with the Latest Technologies...</a>
-    <ul class="navbar-nav ml-auto" style={{color: "white"}}>
+  <a class="navbar-brand font-italic" href="#" style={{color: "white"}} > © Copyright 2015. All Rights Reserved by AnswarIT</a>
+    <ul class="navbar-nav ml-auto" style={{color: "white"}} >
     
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
+        <a class="nav-link" href="#" style={{color: "white" }} >  Terms of Use   </a> 
+      </li>  <span style={{ borderRight:" 1px solid", margin: "auto 10px", height: " 15px"}}> </span>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
+        <a class="nav-link" href="#" style={{color: "white", }}> Sitemap </a>
+      </li>  <span style={{ borderRight:" 1px solid", margin: "auto 10px", height: " 15px"}}> </span>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
+        <a class="nav-link" href="#" style={{color: "white"}}> Contact </a>
+      </li>  
     </ul>
   </div>
 </nav>
