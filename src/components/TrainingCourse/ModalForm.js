643505f3c707b201c80531a6464ce577e1a89ec9
Md-Mainul <mainulislamfaruqi@gmail.com>
2021-06-15 17:21:06 +0600

    update

--- src/components/TrainingCourse/ModalForm.js
+++ src/components/TrainingCourse/ModalForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
-import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import { Link } from "react-router-dom";
 import "./ModalForm.css";
 const ModalForm = ({ id }) => {
   const history = useHistory();
@@ -641,6 +641,21 @@ const ModalForm = ({ id }) => {
           <label for="q10:4_s40" style={{ marginLeft: "5px" }}>
             Uranium
           </label>
+        </fieldset>
+
+        <fieldset>
+          <input
+            onChange={handleChange}
+            type="checkbox"
+            value="Silicon"
+            name="Q10"
+            required
+            id="q10:1_s37"
+          />
+          <label for="q10:1_s37" style={{ marginLeft: "5px" }}>
+            I agree to the <Link to=""> <b style={{color: "rgb(92, 16, 227)"}}> terms and conditions </b> </Link> 
+          </label>
+          <br />
         </fieldset>
 
         {/* this is submit button */}
