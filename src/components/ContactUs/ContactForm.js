8e14a8851632a867c3d38c482bf82efed552f3b9
md jahidul islam <za0162hid@gmail.com>
2021-06-19 00:11:23 +0600

    contact into added in the server

--- src/components/ContactUs/ContactForm.js
+++ src/components/ContactUs/ContactForm.js
@@ -1,52 +1,140 @@
-import React from 'react';
-import './ContactForm.scss';
+import React, { useState } from "react";
+import "./ContactForm.scss";
 
 const ContactForm = () => {
-    return (
-        <div>
-            <form>
-            <div class="form-row">
-    
-    <div class="form-group col-md-6">
-      <label for="inputState"> <b> Choose a Country *</b> </label>
-      <select id="inputState" class="form-control">
-        <option selected> USA </option>
-        <option> Uk  </option>
-        <option> CANADA  </option>
-        <option> AUSTRALIA  </option>
-      </select>
-    </div>
+  const [contactInfo, setContactInfo] = useState([""]);
 
-    <div class="form-group col-md-6">
-      <label for="inputCity"> <b> Your Name * </b></label>
-      <input type="text" class="form-control" id="inputCity" required />
-    </div>
-  </div>
-  <div class="form-row">
-  <div class="form-group col-md-6">
-      <label for="inputPassword4"> <b> Phone *</b> </label>
-      <input type="tel" class="form-control" id="inputPassword4" required />
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputEmail4"><b>Email *</b></label>
-      <input type="email" class="form-control" id="inputEmail4" required />
-    </div>
-    
-  </div>
-  <div class="form-group">
-    <label for="inputAddress"> <b>Subject *</b>  </label>
-    <input type="text" class="form-control" id="inputAddress" placeholder=" " required />
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2"> <b>Message *</b> </label>
-    <textarea type="text" rows="6" class="form-control" id="inputAddress2" placeholder=" " required />
-  </div>
-  
- 
-  <button type="submit" class="btn btn-primary"> Send Message </button>
-</form>
+  const handleChange = (e) => {
+    const userInfo = { ...contactInfo };
+    userInfo[e.target.name] = e.target.value;
+    setContactInfo(userInfo);
+  };
+  console.log(contactInfo, "hey im here");
+  const handleSubmit = (event) => {
+    fetch("http://localhost:5000/addContactInfo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contactInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          console.log(data);
+        }
+      });
+
+    alert("Message Send Successfully");
+    // event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputState">
+              {" "}
+              <b> Choose a Country *</b>{" "}
+            </label>
+            <select
+              name="selected-country"
+              onChange={handleChange}
+              id="inputState"
+              class="form-control"
+            >
+              <option selected> </option>
+              <option> BANGLADESH </option>
+              <option> UK </option>
+              <option> CANADA </option>
+              <option> AUSTRALIA </option>
+            </select>
+          </div>
+
+          <div class="form-group col-md-6">
+            <label for="inputCity">
+              {" "}
+              <b> Your Name * </b>
+            </label>
+            <input
+              onChange={handleChange}
+              name="name"
+              type="text"
+              class="form-control"
+              id="inputCity"
+              required
+            />
+          </div>
         </div>
-    );
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputPassword4">
+              {" "}
+              <b> Phone *</b>{" "}
+            </label>
+            <input
+              onChange={handleChange}
+              name="phone"
+              type="tel"
+              class="form-control"
+              id="inputPassword4"
+              required
+            />
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputEmail4">
+              <b>Email *</b>
+            </label>
+            <input
+              name="email"
+              onChange={handleChange}
+              type="email"
+              class="form-control"
+              id="inputEmail4"
+              required
+            />
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="inputAddress">
+            {" "}
+            <b>Subject *</b>{" "}
+          </label>
+          <input
+            name="subject"
+            onChange={handleChange}
+            type="text"
+            class="form-control"
+            id="inputAddress"
+            placeholder=" "
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="inputAddress2">
+            {" "}
+            <b>Message *</b>{" "}
+          </label>
+          <textarea
+            onChange={handleChange}
+            name="message"
+            type="text"
+            rows="6"
+            class="form-control"
+            id="inputAddress2"
+            placeholder=" "
+            required
+          />
+        </div>
+
+        {/* <button type="submit" class="btn btn-primary">
+          {" "}
+          Send Message{" "}
+        </button> */}
+        <input className="btn btn-primary" type="submit" value="Send Message" />
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
