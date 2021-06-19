f55ce3939fd33d5cf1f7a6911526a9552418d055
md jahidul islam <za0162hid@gmail.com>
2021-06-19 22:24:43 +0600

    admin name responsive

--- src/components/Admin/AdminData.js
+++ src/components/Admin/AdminData.js
@@ -6,28 +6,22 @@ const AdminData = (props) => {
   const { name, image, title, description, desc2 } = props.singleData;
   return (
     <section style={mainSection} className=" container-fluid w-100 mb-5">
-        <div className="inner-section row">
-            <div className="col-lg-4 col-md-12 img-container">
-                <img className="img-fluid  img" src={image} alt="" />
-            </div>
+      <div className="inner-section row">
+        <div className="col-lg-4 col-md-12 img-container">
+          <img className="img-fluid  img" src={image} alt="" />
+        </div>
         <div className="col-md-12 col-lg-8">
-            <h2
-            style={{ color: "#5c10e3", lineHeight: "17px" }}
-            className="nameDesign"
-            >
+          <h2 style={{ color: "#5c10e3" }} className="nameDesign">
             {name}
-            </h2>
-            <h5 style={{ margin:"1rem 0"}}>{title}</h5>
-            <p style={{ lineHeight: "30px", textAlign:"justify"}}>
-                {description}
-            </p>
+          </h2>
+          <h5 style={{ margin: "1rem 0" }}>{title}</h5>
+          <p style={{ lineHeight: "30px", textAlign: "justify" }}>
+            {description}
+          </p>
 
-            <p style={{ lineHeight: "30px", textAlign:"justify" }}>
-                {desc2}
-            </p>
+          <p style={{ lineHeight: "30px", textAlign: "justify" }}>{desc2}</p>
         </div>
-        </div>
-      
+      </div>
     </section>
   );
 };
